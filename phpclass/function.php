f0a73f300cb8fe18e475a15f3a0c2b7b3f077daa
shiva-295 <kamlasramsaro@gmail.com>
2025-08-20 12:05:42 +0530

    new

---
+++ phpclass/function.php
@@ -0,0 +1,72 @@
+<?php
+
+
+// function userDetails(){
+//     echo "Hello Kamlesh!";
+// }
+
+// userDetails();
+// userDetails();
+// userDetails();
+// userDetails();
+
+
+// function calculator($a, $b, $operator)
+// {
+
+//     switch ($operator) {
+//         case "+":
+//             echo $a + $b;
+//             break;
+//         case "-":
+//             echo $a - $b;
+//             break;
+//         case "*":
+//             echo $a * $b;
+//             break;
+//         case "/":
+//             echo $a / $b;
+//             break;
+//     }
+// }
+
+
+// calculator(2,3,"-");
+// calculator(2,3,"+");
+// calculator(2,3,"/");
+// calculator(2,3,"*");
+
+
+// function add(){ # 9
+// return 5 + 4; # 5 + 4 = 9
+// }
+
+// echo add();
+
+// function name() {
+//     echo "shiva";
+
+// }
+// name();
+
+function name($a, $b, $operator)
+{
+    
+
+    // $operator = "+";
+    switch ($operator) {
+        case "+":
+            echo $a + $b;
+            break;
+        case "-":
+            echo $a - $b;
+            break;
+        case "*":
+            echo $a * $b;
+            break;
+        case "/":
+            echo $a / $b;
+            break;
+    }
+}
+name(10,29,"-");
